4da082077191bfc3f34b28d3a6fdee765873a470
Troy Dai <git@troydai.com>
2024-08-19 20:16:09 -0700

    Accept connection in workers

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -24,10 +27,38 @@ func main() {
 		os.Exit(0)
 	}()
 
-	conn, err := lis.Accept()
-	if err != nil {
-		log.Fatalf("failed to accept: %v", err)
-	}
+	// next client ID, the accept is run in a loop therefore no race condition
+	clientID := 0
 
-	log.Println("Accepted connection from", conn.RemoteAddr())
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Fatalf("failed to accept: %v", err)
+		}
+
+		workerID := fmt.Sprintf("worker-%d", clientID)
+		clientID++
+		log.Printf("Workder %s accepted connection from %s", workerID, conn.RemoteAddr())
+
+		// worker
+		go func() {
+			for {
+				conn.Write([]byte("Hello from " + workerID + "\n"))
+
+				buf := make([]byte, 1024)
+				i, err := conn.Read(buf)
+				if errors.Is(err, io.EOF) {
+					log.Println("Connection closed by client")
+					// exit the worker
+					return
+				} else if err != nil {
+					log.Println("Unexpected error:", err)
+					// exit the worker
+					return
+				}
+
+				log.Print("Received:", string(buf[:i]))
+			}
+		}()
+	}
 }
